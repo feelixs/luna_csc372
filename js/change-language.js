c5c320e1e812791cc3bdc8f8e84654287ae21fbc
Michael Felix <mikemh@uri.edu>
2024-02-06 13:55:34 -0500

    change langague logic

---
+++ js/change-language.js
@@ -0,0 +1,38 @@
+var currentLang = 'en';
+
+
+function toggleLanguage() {
+    /*
+       Swaps between Spanish and English, and update the page's text to the new lang
+    */
+    let templang = 'en';
+    if (currentLang === 'en') {
+        templang = 'es';
+    }
+    currentLang = templang;
+
+    document.documentElement.lang = currentLang; // update the page's lang attribute
+    applyNavLanguageChange(currentLang);
+    applyMainLanguageChange(currentLang)
+}
+
+
+function applyNavLanguageChange(newLang) {
+    /*
+        Applies language change between english and spanish for the navigation bar's text
+    */
+    const changeLangImg = document.getElementById('change-language');
+
+    if (newLang === 'es') {  // toggle the image between saying 'eng' and 'esp'
+        changeLangImg.src = `images/buttons/globe-white-en.webp`;
+        changeLangImg.alt = `Button that changes language to English`;
+    } else {
+        changeLangImg.src = `images/buttons/globe-white-es.webp`;
+        changeLangImg.alt = `Button that changes language to Spanish`;
+    }
+
+    // manually update the nav's text fields
+    document.getElementById('home-nav').innerHTML = newLang === 'es' ? 'Inicio' : 'Home';
+    document.getElementById('bios-nav').innerHTML = newLang === 'es' ? 'Sobre' : 'About';
+    document.getElementById('media-nav').innerHTML = newLang === 'es' ? 'Medios' : 'Media';
+}
