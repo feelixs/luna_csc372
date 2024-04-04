52658c3c842cc945ee22fdc1970cf83dac3b9b58
Michael Felix <mikemh@uri.edu>
2024-04-04 11:32:51 -0400

    js paths

--- js/contact.js
+++ js/contact.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
     /*
         Load this page's content in the default language when the page loads.
     */
-    cntTextFields.push(new XMLDualLangTextField(`/data/xml/contact.xml`, 0, $('#contact-desc')));
-    cntTextFields.push(new HTMLDualLangTextField(`/data/html/footer`, $('#footer-text')));
-    cntTextFields.push(new HTMLDualLangTextField( `/data/html/copyright`, $('#copyright')));
-    cntDualImages.push(new DualLangImage('/images/buttons/globe-white-en.webp',
-        '/images/buttons/globe-white-es.webp', $('#change-language-img')))
+    cntTextFields.push(new XMLDualLangTextField(`../data/xml/contact.xml`, 0, $('#contact-desc')));
+    cntTextFields.push(new HTMLDualLangTextField(`../data/html/footer`, $('#footer-text')));
+    cntTextFields.push(new HTMLDualLangTextField( `../data/html/copyright`, $('#copyright')));
+    cntDualImages.push(new DualLangImage('../images/buttons/globe-white-en.webp',
+        '../images/buttons/globe-white-es.webp', $('#change-language-img')))
 
     // load the current langague from the document's 'lang' attribute, which was set by the server (django)
     var currentLang = $('html').attr('lang');
