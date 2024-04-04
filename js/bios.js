a04d6f402842237a45fadfb8b4acc5d888491c37
Michael Felix <mikemh@uri.edu>
2024-04-04 11:35:46 -0400

    js paths

--- js/bios.js
+++ js/bios.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
     /*
         Load this page's content in the default language when the page loads.
     */
-    biosTextFields.push(new XMLDualLangTextField(`/data/xml/bios.xml`, 0, $('#carmen-bio'), '&ensp;&ensp;&ensp;&ensp;'));
-    biosTextFields.push(new XMLDualLangTextField(`/data/xml/bios.xml`, 1, $('#marco-bio'), '&ensp;&ensp;&ensp;&ensp;'));
-    biosTextFields.push(new XMLDualLangTextField(`/data/xml/bios.xml`, 2, $('#nicolas-bio'), '&ensp;&ensp;&ensp;&ensp;'));
-    biosTextFields.push(new HTMLDualLangTextField(`/data/html/footer`, $('#footer-text')));
-    biosTextFields.push(new HTMLDualLangTextField( `/data/html/copyright`, $('#copyright')));
-    bioDualImages.push(new DualLangImage('/images/buttons/globe-white-en.webp',
-        '/images/buttons/globe-white-es.webp', $('#change-language-img')))
+    biosTextFields.push(new XMLDualLangTextField(`data/xml/bios.xml`, 0, $('#carmen-bio'), '&ensp;&ensp;&ensp;&ensp;'));
+    biosTextFields.push(new XMLDualLangTextField(`data/xml/bios.xml`, 1, $('#marco-bio'), '&ensp;&ensp;&ensp;&ensp;'));
+    biosTextFields.push(new XMLDualLangTextField(`data/xml/bios.xml`, 2, $('#nicolas-bio'), '&ensp;&ensp;&ensp;&ensp;'));
+    biosTextFields.push(new HTMLDualLangTextField(`data/html/footer`, $('#footer-text')));
+    biosTextFields.push(new HTMLDualLangTextField( `data/html/copyright`, $('#copyright')));
+    bioDualImages.push(new DualLangImage('images/buttons/globe-white-en.webp',
+        'images/buttons/globe-white-es.webp', $('#change-language-img')))
 
     // load the current langague from the document's 'lang' attribute, which was set by the server (django)
     var currentLang = $('html').attr('lang');
