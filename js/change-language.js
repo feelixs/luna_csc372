ec18adfebdc28af3fe72d4f1c5f01bce4ac0a6dd
Michael Felix <mikemh@uri.edu>
2024-04-11 12:12:01 -0400

    Update change-language.js

--- js/change-language.js
+++ js/change-language.js
@@ -3,6 +3,23 @@
 var currentLang = "en";
 
 
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+
 function toggleLanguage() {
     /*
        Swaps between Spanish and English, and update the page's text to the new lang
