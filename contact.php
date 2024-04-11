c5d637bd45d3c6a9e6b6ecde75a517c038fb29cb
Michael Felix <mikemh@uri.edu>
2024-04-11 12:10:32 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,12 +1,21 @@
 <?php
+function setLanguageCooke(string $language) {
+    setcookie("language", $language, time() + (24 * 60 * 60 * 30), "/"); // 30 days
+}
 
-include 'php_include/language.php';
+function getLanguageCookie() {
+    return $_COOKIE["language"];
+}
 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    setLanguageCooke($_POST["language"]);
+}
 
 ?>
 
 <!doctype html>
-<html lang="<?= getLanguageCookie() ?>">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -34,7 +43,7 @@ include 'php_include/language.php';
             <li><a id="media-nav" href="gallery.php">Media</a></li>
             <li><a id="patreon-nav" href="#">Contact</a></li>
             <li class="lang-chg trans-17">
-                <form id="language-form" action="php_include/language.php" method="POST">
+                <form id="language-form" action="contact.php" method="POST">
                     <input type="hidden" name="language" id="language-input">
                     <button type="submit"><img id="change-language-img" alt="Button that changes the page's language" src="images/buttons/globe-white-es.webp"></button>
                 </form>
