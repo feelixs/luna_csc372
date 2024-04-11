4786a626901a2f37d079ce6c6197747611593f0e
Michael Felix <mikemh@uri.edu>
2024-04-11 14:05:27 -0400

    Revert "Update contact.js"
    This reverts commit fde8e55b7012c5c038d529112ca2501c0b0e4c9c.

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,6 @@
 var cntTextFields = []
 var cntDualImages = []
-var $langForm = $('#language-form');
-var $langInput = $('#language-input');
+var $langbtn = $('#language-form');
 
 $(document).ready(function () {
     /*
@@ -19,20 +18,15 @@ $(document).ready(function () {
     loadContentInLang(currentLang);
 })
 
-
-$langForm.on('submit', function(e) {
-    e.preventDefault(); // Prevent form from submitting immediately
-
-    let oldLang = $('html').attr('lang'); // Get the current language from the HTML lang attribute
-
-    // Determine the new language based on the old one
-    let newLang = (oldLang === 'en') ? 'es' : 'en';
-
-    // Set the new language in the hidden input
-    $langInput.val(newLang);
-
-    // Now submit the form programmatically since we've prevented the default submit action
-    this.submit(); // `this` refers to the form element in this context
+$langbtn.on('submit', function(e) {
+    let oldLang = $('html').attr('lang'); // get the current language (the actual currentLang var might be out of date)
+    if (oldLang === 'en') {
+        currentLang = 'es';
+    } else if (oldLang === 'es') {
+        currentLang = 'en';
+    }
+    e.preventDefault();
+    $langbtn.val(currentLang);
 });
 
 function applyMainLanguageChange(newlang) {
