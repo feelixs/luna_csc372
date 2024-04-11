b47fac6e03c96eacf02dbc54e7cdd71f833976d3
Michael Felix <mikemh@uri.edu>
2024-04-11 11:19:18 -0400

    test php

--- contact.php
+++ contact.php
@@ -1,3 +1,9 @@
+<?php
+
+$name = "Michae Felix!!"
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -71,7 +77,7 @@
         </div>
     </div>
     <div class="flex-container" style="padding-bottom: 20px" id="footer-text">
-        If you have any questions or concerns, please contact Michael Felix through <a class="email-link"
+        If you have any questions or concerns, please contact <?= $name ?> through <a class="email-link"
                                                                                        target="_blank"
                                                                                        href="mailto:mikemh@uri.edu">mikemh@uri.edu</a>
     </div>
