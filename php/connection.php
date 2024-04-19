fedde9b16bde2a5c444923436495263c21ecf087
Michael Felix <mikemh@uri.edu>
2024-04-19 14:02:13 -0400

    Revert "Update connection.php"
    This reverts commit 538830c90a92c242b72ec0b9e5e4c832be12a8c8.

--- php/connection.php
+++ php/connection.php
@@ -62,7 +62,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $query = $pdo->prepare($sql);
         $query->execute($params);
 
-     
+        header("Location: ../contact.php?status=200");
+        exit();
     }
 }
 
