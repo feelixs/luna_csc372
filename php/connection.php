746f67f676bc1f8caf12ae9fbc86d0eb20359447
Michael Felix <mikemh@uri.edu>
2024-04-19 15:06:01 -0400

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -61,10 +61,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         $query = $pdo->prepare($sql);
         $query->execute($params);
-
-        header("Location: ../login.php");
-        exit();
     }
+    header("Location: ../login.php");
+    exit();
 }
 
 ?>
