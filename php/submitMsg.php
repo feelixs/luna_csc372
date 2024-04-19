da489ff69e82cd4a139cd064d4e63adb031762ce
Michael Felix <mikemh@uri.edu>
2024-04-19 12:36:07 -0400

    post sql

--- php/submitMsg.php
+++ php/submitMsg.php
@@ -1,7 +1,18 @@
 <?php
 
+require 'php/connection.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO messages (user, MESSAGE_TEXT, timestamp) VALUES ('$email', '$message', NOW())"
+    $statement['user'] = $email;
+    $statement['MESSAGE_TEXT'] = $message;
+    $statement['timestamp'] = NOW();
+
+    $query = $pdo->prepare($sql);
+    $query->execute($statement);
 
     header("Location: ../contact.php?status=200");
     exit();
