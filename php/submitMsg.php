6eaf5dcfb8f456fd029c84279da9b07a63daf6b2
Michael Felix <mikemh@uri.edu>
2024-04-19 12:32:29 -0400

    Update submitMsg.php

--- php/submitMsg.php
+++ php/submitMsg.php
@@ -2,10 +2,7 @@
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $message = $_POST['message'];
 
-    $statement = $pdo->prepare("INSERT INTO messages (user, MESSAGE_TEXT, timestamp) VALUES ('$email', '$message', NOW())");
     header("Location: ../contact.php?status=200");
     exit();
 }
