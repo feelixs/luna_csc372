d3942976c916ac1ffb5c36cf61273b9cd3423584
Michael Felix <mikemh@uri.edu>
2024-04-19 12:42:56 -0400

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -50,16 +50,6 @@ function pdo(PDO $pdo, string $sql, array $arguments = null)
 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $sql = "INSERT INTO messages (user, MESSAGE_TEXT, timestamp) VALUES ('$email', '$message', NOW())"
-    $statement['user'] = $email;
-    $statement['MESSAGE_TEXT'] = $message;
-    $statement['timestamp'] = NOW();
-
-    $query = $pdo->prepare($sql);
-    $query->execute($statement);
 
     header("Location: ../contact.php?status=200");
     exit();
